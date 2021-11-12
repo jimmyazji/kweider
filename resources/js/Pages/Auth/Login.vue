ba9a8284e918f11ae2f9c8c4408ca8a629b403c3
Jimmy <jimmyyazji98@gmail.com>
2021-11-12 21:31:28 +0200

    implemented daisyUI

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,85 +1,146 @@
 <template>
-    <Head title="Log in" />
+  <Head title="Log in" />
 
-    <BreezeValidationErrors class="mb-4" />
+  <BreezeValidationErrors class="mb-4" />
 
-    <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-        {{ status }}
+  <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+    {{ status }}
+  </div>
+
+  <form @submit.prevent="submit">
+    <div>
+      <BreezeInput
+        id="email"
+        type="email"
+        class="mt-1 block w-full"
+        v-model="form.email"
+        required
+        autofocus
+        autocomplete="username"
+        placeholder="Email"
+      />
     </div>
 
-    <form @submit.prevent="submit">
-        <div>
-            <BreezeLabel for="email" value="Email" />
-            <BreezeInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus autocomplete="username" />
-        </div>
+    <div class="mt-2 relative">
+      <BreezeInput
+        id="password"
+        :type="passwordFieldType"
+        class="mt-1 block w-full"
+        v-model="form.password"
+        required
+        autocomplete="current-password"
+        placeholder="Password"
+      />
+      <button
+        @click.prevent="switchVisibility"
+        class="absolute top-0 right-0 rounded-l-none btn btn-primary"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#fff"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="h-6 w-auto"
+        >
+          <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+          <circle cx="12" cy="12" r="3" />
+        </svg>
+      </button>
+    </div>
 
-        <div class="mt-4">
-            <BreezeLabel for="password" value="Password" />
-            <BreezeInput id="password" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="current-password" />
-        </div>
+    <div class="flex items-center justify-between mt-4">
+      <label class="flex items-center">
+        <BreezeCheckbox name="remember" v-model:checked="form.remember" />
+        <span class="ml-2 text-sm text-lonestar-700">Remember me</span>
+      </label>
+      <Link
+        v-if="canResetPassword"
+        :href="route('password.request')"
+        class="
+          hover:underline
+          text-sm text-lonestar-700
+          hover:text-lonestar-900
+        "
+      >
+        Forgot password?
+      </Link>
+    </div>
+    <div class="flex flex-wrap justify-center mt-2">
+      <BreezeButton
+        :class="{ 'opacity-25': form.processing }"
+        :disabled="form.processing"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#FFF"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="w-4 h-4 mr-2 text-white"
+        >
+          <path
+            d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4m-5-4 5-5-5-5m5 5H3"
+          />
+        </svg>
 
-        <div class="block mt-4">
-            <label class="flex items-center">
-                <BreezeCheckbox name="remember" v-model:checked="form.remember" />
-                <span class="ml-2 text-sm text-gray-600">Remember me</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900">
-                Forgot your password?
-            </Link>
-
-            <BreezeButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                Log in
-            </BreezeButton>
-        </div>
-    </form>
+        Log in
+      </BreezeButton>
+    </div>
+  </form>
 </template>
 
 <script>
-import BreezeButton from '@/Components/Button.vue'
-import BreezeCheckbox from '@/Components/Checkbox.vue'
-import BreezeGuestLayout from '@/Layouts/Guest.vue'
-import BreezeInput from '@/Components/Input.vue'
-import BreezeLabel from '@/Components/Label.vue'
-import BreezeValidationErrors from '@/Components/ValidationErrors.vue'
-import { Head, Link } from '@inertiajs/inertia-vue3';
+import BreezeButton from "@/Components/Button.vue";
+import BreezeCheckbox from "@/Components/Checkbox.vue";
+import BreezeGuestLayout from "@/Layouts/Guest.vue";
+import BreezeInput from "@/Components/Input.vue";
+import BreezeLabel from "@/Components/Label.vue";
+import BreezeValidationErrors from "@/Components/ValidationErrors.vue";
+import { Head, Link } from "@inertiajs/inertia-vue3";
 
 export default {
-    layout: BreezeGuestLayout,
+  layout: BreezeGuestLayout,
 
-    components: {
-        BreezeButton,
-        BreezeCheckbox,
-        BreezeInput,
-        BreezeLabel,
-        BreezeValidationErrors,
-        Head,
-        Link,
+  components: {
+    BreezeButton,
+    BreezeCheckbox,
+    BreezeInput,
+    BreezeLabel,
+    BreezeValidationErrors,
+    Head,
+    Link,
+  },
+
+  props: {
+    canResetPassword: Boolean,
+    status: String,
+  },
+
+  data() {
+    return {
+      passwordFieldType: "password",
+      form: this.$inertia.form({
+        email: "",
+        password: "",
+        remember: false,
+      }),
+    };
+  },
+
+  methods: {
+    submit() {
+      this.form.post(this.route("login"), {
+        onFinish: () => this.form.reset("password"),
+      });
     },
-
-    props: {
-        canResetPassword: Boolean,
-        status: String,
-    },
-
-    data() {
-        return {
-            form: this.$inertia.form({
-                email: '',
-                password: '',
-                remember: false
-            })
-        }
-    },
-
-    methods: {
-        submit() {
-            this.form.post(this.route('login'), {
-                onFinish: () => this.form.reset('password'),
-            })
-        }
+    switchVisibility(){
+      this.passwordFieldType = this.passwordFieldType === "password" ? "text" :"password";
     }
-}
+  },
+};
 </script>
