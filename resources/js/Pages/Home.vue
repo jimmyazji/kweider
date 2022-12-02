29dc7575baf1a8d863821f1f662db2059a19a4d2
jimmyazji <jimmyyaji98@gmail.com>
2022-12-02 21:21:16 +0300

    removed featured products

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -3,24 +3,15 @@
     <div>
         <img src="/images/asawer-walnut.gif" alt />
     </div>
-    <div class="max-w-7xl mx-auto py-10">
+    <!-- <div class="max-w-7xl mx-auto py-10">
         <h4 class="text-3xl text-lonestar-500 py-5 font-semibold">
             Featured Products
         </h4>
-    </div>
+    </div> -->
 </template>
 
 <script setup>
-import { Head, Link } from "@inertiajs/inertia-vue3";
+import { Head } from "@inertiajs/inertia-vue3";
 import "vue3-carousel/dist/carousel.css";
-import { Carousel, Slide, Navigation, Pagination } from "vue3-carousel";
-const breakpoints = {
-    700: {
-        itemsToShow: 3.5,
-    },
-    1024: {
-        itemsToShow: 5,
-    },
-};
 </script>
 <style scoped></style>
