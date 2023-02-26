0eb8cc730524ab2c986c49c4406f4ddd7d738ea0
jimmyazji <jimmyyazji@gmail.com>
2023-02-26 21:12:38 +0000

    Fixed autherization to horizon page.

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -30,7 +30,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     protected function gate(): void
     {
         Gate::define('viewHorizon', function ($user) {
-            $user->can('view-horizon');
+            return $user->can('view-horizon');
         });
     }
 }
